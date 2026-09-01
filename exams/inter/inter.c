b229c16af9ff26cde2b71b5e7e5b8751b4bf0bc3
saiyengit <niyserdu13@gmail.com>
2026-09-01 10:05:40 +0200

    m

--- exams/inter/inter.c
+++ exams/inter/inter.c
@@ -12,35 +12,34 @@ int	main(int argc, char **argv)
 	int	id;
 	int	trouve;
 
-	i = 0;
+	i2 = 0;
 	if (argc != 3)
 	{
 		ft_putchar('\n');
 		return (0);
 	}
+	while (argv[2][i2])
+	{
+		if (argv[1][0] == argv[2][i2])
+			ft_putchar(argv[1][0]);
+		i2++;
+	}
+	i = 1;
 	while (argv[1][i])
 	{
-		trouve = 0;
 		i2 = 0;
 		while (argv[2][i2])
 		{
 			if (argv[1][i] == argv[2][i2])
 			{
-				id = i - 1;
+				id = 0;
 				while (id < i && trouve == 0)
 				{
 					if (argv[1][id] == argv[1][i])
-						trouve = 1;
+						trouve == 1;
 					id++;
 				}
-				id = i2 - 1;
-				while (id < i2 && trouve == 0)
-				{
-					if (argv[2][id] == argv[2][i2])
-						trouve = 1;
-					id++;
-				}
-				if (trouve == 0)
+				if (trouve == 1)
 					ft_putchar(argv[1][i]);
 			}
 			i2++;
