4da278f72dea705bfbac16c80c1cfab6de11c986
saiyengit <niyserdu13@gmail.com>
2026-09-13 01:32:51 +0200

    m

---
+++ rosetta/ex00/rosetta.h
@@ -0,0 +1,8 @@
+#ifndef ROSETTA_H
+# define ROSETTA_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
---
+++ rosetta/ex00/utils.c
@@ -0,0 +1,57 @@
+#include "rosetta.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_isdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\t')
+		return (1);
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+char	*strdup(char *src)
+{
+	char	*copy;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(src);
+	copy = malloc(sizeof(char) * (len + 1));
+	if (copy == 0)
+		return (NULL);
+	while (src[i])
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
