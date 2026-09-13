f7055da939de7c65ae666051c59643f1e50e3e91
saiyengit <niyserdu13@gmail.com>
2026-09-13 02:15:40 +0200

    m

--- rosetta/ex00/rosetta.h
+++ rosetta/ex00/rosetta.h
@@ -5,4 +5,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+int		ft_strlen(char *str);
+int		ft_isdigit(char c);
+int		ft_isspace(char c);
+void	ft_putstr(char *str);
+char	*ft_strdup(char *src);
+int		ft_strcmp(char *s1, char *s2);
+int		is_valid_number(char *str);
+int		ft_strjoinlen(int size, char **strs, char *sep);
+void	ft_strjoin_fill(char *res, int size, char **strs, char *sep);
+char	*ft_strjoin(int size, char **strs, char *sep);
+
 #endif
---
+++ rosetta/ex00/utils2.c
@@ -0,0 +1,84 @@
+#include "rosetta.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '+')
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_strjoinlen(int size, char **strs, char *sep)
+{
+	int	totalstrssep;
+	int	i;
+
+	i = 0;
+	totalstrssep = 0;
+	if (size == 0)
+		return (0);
+	while (i < size)
+	{
+		totalstrssep = totalstrssep + ft_strlen(strs[i]);
+		if (i < size - 1)
+			totalstrssep = totalstrssep + ft_strlen(sep);
+		i++;
+	}
+	return (totalstrssep);
+}
+
+void	ft_strjoin_fill(char *res, int size, char **strs, char *sep)
+{
+	int	i;
+	int	i2;
+	int	i3;
+	int	ir;
+
+	i = 0;
+	ir = 0;
+	while (i < size)
+	{
+		i2 = 0;
+		while (strs[i][i2])
+			res[ir++] = strs[i][i2++];
+		if (i < size - 1)
+		{
+			i3 = 0;
+			while (sep[i3])
+				res[ir++] = sep[i3++];
+		}
+		i++;
+	}
+	res[ir] = '\0';
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*res;
+
+	res = malloc(sizeof(char) * (ft_strjoinlen(size, strs, sep) + 1));
+	if (res == 0)
+		return (NULL);
+	ft_strjoin_fill(res, size, strs, sep);
+	return (res);
+}
